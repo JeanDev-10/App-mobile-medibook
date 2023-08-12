8e7f85f9b846312ca1fb68b6f76dbf4abed9b04d
Jesus <133887362+JessEleven@users.noreply.github.com>
2023-08-12 14:27:27 -0500

    landing, login and register

--- src/app/public/pages/login/login.page.ts
+++ src/app/public/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  
+  formLogin!:FormGroup
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService,
+    private formBuilder: FormBuilder) {
+    this.formLogin= this.formBuilder.group({
+      email:['', [ 
+        Validators.required, 
+        Validators.email
+        ]],
 
-  ngOnInit() {
+      password:['', [
+        Validators.required, 
+        Validators.minLength(5), 
+        Validators.maxLength(10)
+      ]],
+    })
   }
 
+  Login(form: any) {
+    if (this.formLogin.invalid) {
+      // Marcar los campos del formulario como tocados para mostrar los mensajes de error
+      Object.values(this.formLogin.controls).forEach((control) =>
+      control.markAsTouched(),
+      );
+      return;
+    } else {
+      this.authService.login(form).subscribe((data) => {
+        this.authService.setToken(data.access_token);
+        this.router.navigate(['/home']);
+      });
+    }
+  }
+
+  ngOnInit() {}
 }
--- src/app/public/pages/register/register.page.ts
+++ src/app/public/pages/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  formRegister!:FormGroup
 
-  ngOnInit() {
+  provincias_array = ['Azuay','Bolívar','Cañar','Carchi','Chimborazo','Cotopaxi','El Oro','Esmeraldas','Galápagos','Guayas','Imbabura','Loja','Los Ríos','Manabí','M. Santiago','Napo','Orellana','Pastaza','Pichincha','Santa Elena','S.D. de los Tsáchilas','Sucumbios','Tungurahua','Zamora Chinchipe']
+
+
+  canton_array = [
+    'Cantón Portoviejo','Cantón 24 de Mayo','Cantón Bolívar','Cantón Chone','Cantón El Carmen',
+    'Cantón Flavio Alfaro','Cantón Jama','Cantón Jaramijó','Cantón Jipijapa','Cantón Junín',
+    'Cantón Manta','Cantón Montecristi','Cantón Olmedo','Cantón Paján','Cantón Pedernales',
+    'Cantón Pichincha','Cantón Puerto López','Cantón Rocafuerte',
+    'Cantón San Vicente','Cantón Santa Ana','Cantón Sucre','Cantón Tosagua'
+  ];
+
+  constructor(private router: Router, private authService: AuthService,
+    private formBuilder: FormBuilder) {
+    this.formRegister = this.formBuilder.group({
+      email:['', [Validators.required, Validators.email]],
+      password:['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+
+      nombre:['',[Validators.required,Validators.minLength(3)]],
+      apellido: ['',[Validators.required,Validators.minLength(3)]],
+      
+      edad: ['', [Validators.required, Validators.pattern(/^-?(1|[1-9]\d*)?$/)]],
+      ci: ['', [Validators.required, Validators.pattern("^[0-9]{10}$")]],
+      telefono: ['', [Validators.required, Validators.pattern("^[0-9]{10}$")]],
+      fecha: ['', Validators.required],
+
+      canton: ['', [Validators.required, Validators.minLength(3)]],
+      'provincia' : ['', [Validators.required, Validators.minLength(3)]]
+      
+    })
   }
 
+  Register(form: any) {
+    if (this.formRegister.invalid) {
+      // Marcar los campos del formulario como tocados para mostrar los mensajes de error
+      Object.values(this.formRegister.controls).forEach((control) =>
+      control.markAsTouched(),
+      );
+      return;
+    } else {
+      this.authService.login(form).subscribe((data) => {
+        this.authService.setToken(data.access_token);
+        this.router.navigate(['patient/home']);
+      });
+    }
+  }
+  
+  ngOnInit() {}
 }
