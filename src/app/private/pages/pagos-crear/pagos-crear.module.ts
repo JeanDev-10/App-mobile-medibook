e5e29b17351a4e9f4681e8e0803f39526e8b08f6
Oliver-08-ojla <oliver2018es@gmail.com>
2023-08-13 15:15:09 -0500

    page pago lista

--- src/app/private/pages/pagos-crear/pagos-crear.module.ts
+++ src/app/private/pages/pagos-crear/pagos-crear.module.ts
@@ -2,18 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { IonicModule } from '@ionic/angular';
 
 import { PagosCrearPageRoutingModule } from './pagos-crear-routing.module';
 
 import { PagosCrearPage } from './pagos-crear.page';
 
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
+
+export function playerFactory() {
+  return player;
+}
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    PagosCrearPageRoutingModule
+    PagosCrearPageRoutingModule,
+    LottieModule.forRoot({ player: playerFactory }),
+    ReactiveFormsModule
+    
   ],
   declarations: [PagosCrearPage]
 })
