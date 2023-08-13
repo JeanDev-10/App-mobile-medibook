8696fc148a4f308f4563b6034d7dcdb09f0d9f6b
Celorio2001 <jahir.celorio2001@gmail.com>
2023-08-12 20:51:19 -0500

    Crud de diseño de la page de citas medicas falta las respectivas validaciones

--- src/app/private/pages/cita-medicas-detalle/cita-medicas-detalle.page.ts
+++ src/app/private/pages/cita-medicas-detalle/cita-medicas-detalle.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cita-medicas-detalle',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitaMedicasDetallePage implements OnInit {
 
-  constructor() { }
+
+  FormCitasMedicasEditar!:FormGroup;
+
+  StateForm = false;
+
+  constructor(private formBuilder:FormBuilder,private router:Router, private toastController:ToastController) {
+    this.FormCitasMedicasEditar = this.formBuilder.group({
+      titulo : new FormControl('', Validators.required),
+      hora_inicio : new FormControl('',Validators.required),
+      hora_fin : new FormControl('',Validators.required),
+      medico_id : new FormControl('',Validators.required)
+    })
+   }
 
   ngOnInit() {
+  }
+
+  // Método creado para hacer el delete del medicamento
+  DeleteMedicament(){
+    this.router.navigate(['citas-medicas'])
+    // Toast de ionic
+    this.presentToast('bottom');
+  }
+
+
+  // Se podría mejorar el toast con un diseño mejor
+
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Cita Medica, Eliminado Correctamente!',
+      duration: 2000,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+  // Cambiar el estado del formulario
+
+  ChangeStateForm(){
+    this.StateForm = !this.StateForm;
+  }
+
+  // Se podría hacer ya el edit método
+  EditMecament(){
+    this.ChangeStateForm();
+  }
+
+  // Método para Actualizar la cita medica por el momento solo se envia los datos por la consola del formulario
+
+  UpdateMedicament(Form:any){
+    console.log(Form);
   }
 
 }
--- src/app/private/pages/citas-medicas-crear/citas-medicas-crear.page.ts
+++ src/app/private/pages/citas-medicas-crear/citas-medicas-crear.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-citas-medicas-crear',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitasMedicasCrearPage implements OnInit {
 
-  constructor() { }
+  FormularioCitasMedicas!:FormGroup;
+
+  options: AnimationOptions = {
+    path: '/assets/anim/animacion_reservar-cita.json',
+  };
+
+  constructor(private formBuilder:FormBuilder) {
+    this.FormularioCitasMedicas = this.formBuilder.group({
+      titulo : new FormControl('',Validators.required),
+      hora_inicio : new FormControl('',Validators.required),
+      hora_fin : new FormControl('',Validators.required),
+      medico_id : new FormControl('',Validators.required)
+    });
+   }
 
   ngOnInit() {
   }
 
+  // Por el momento solo muestro por consola
+  CreateCitaMedica(Form:any){
+    console.log(Form);
+  }
 }
