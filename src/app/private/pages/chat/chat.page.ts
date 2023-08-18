98a0c74bcfda0f4c962dd7bc873dc0f707285051
Jean Rodriguez <jeanpierrerodriguezzambrano@gmail.com>
2023-08-18 01:19:53 -0500

    chat listo

--- src/app/private/pages/chat/chat.page.ts
+++ src/app/private/pages/chat/chat.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { io } from 'socket.io-client';
+import { AuthService } from 'src/app/public/services/auth.service';
 
 @Component({
   selector: 'app-chat',
@@ -6,12 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
-
-  constructor() { }
-
+  private socket: any;
+  messages: any[] = [];
+  newMessage: string = '';
+  user!:any;
+  email!:string;
+  nombre!:string
+  photoURL!:string
+  constructor(private authService:AuthService) {
+    this.authService.userInformation().subscribe((data)=>{
+      console.log(data)
+      this.user=data;
+      this.email=data.email;
+      this.nombre=data.nombre
+    })
+    }
   ngOnInit() {
+    this.socket = io('http://localhost:3000'); // Cambia la URL según tu configuración
+    this.socket.on('message', (data: any) => {
+      console.log(data)
+      this.messages.push(data);
+    });
   }
 
-  
+  sendMessage() {
+    console.log("emitir evento de chat")
+    if (this.newMessage.trim() !== '') {
+      const messageData = {
+        text: this.newMessage,
+        senderName:this.user.nombre,
+      };
+      this.socket.emit('message',messageData );
+      this.newMessage = '';
+      console.log("peticion de envio de mensaje a bd")
+    }
+  }
+
+
+
 
 }
--- src/app/private/pages/titulos/titulos.page.ts
+++ src/app/private/pages/titulos/titulos.page.ts
@@ -8,7 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TitulosPage implements OnInit {
   @Input() medico!:any;
   titulo!:any;
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
   }
